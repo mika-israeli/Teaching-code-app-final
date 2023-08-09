d96acd595c0a0a356b2a551d27a2efe7fba70a04
mika-israeli <mika80666@gmail.com>
2023-08-09 10:21:42 +0300

    NOWw8

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/code", codeRoutes);
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 const MAX_STUDENTS_PER_CLASS = 1;
 
 const whitelist = ["*"];
